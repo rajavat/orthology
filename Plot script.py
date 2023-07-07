a13a3f118a29f0b0ee37e88d8e68949f77ba3134
rajavat <zcbtvsr@ucl.ac.uk>
2023-07-07 11:47:44 +0100

    Initialise

---
+++ Plot script.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def orthoplot(data, speciesA, speciesB, titleA, titleB):
+    plt.rcParams['figure.figsize'] = [8, 8]
+    sns.set_style("whitegrid")
+    sns.scatterplot(data = data, x = speciesA, y = speciesB, 
+                size = 'Orthologs', sizes = (10, 200),
+                hue = 'Orthologs', palette = "crest")
+
+    plt.xlabel(titleA, fontsize = 13, 
+           labelpad = 15, style = 'italic')
+    plt.ylabel(titleB, fontsize = 13, 
+           labelpad = 15, style = 'italic')
+
+    plt.xticks(rotation='vertical', fontsize = 9)
+    plt.yticks(fontsize = 8)
+
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper left', 
+           fontsize=10, title = 'Orthologous genes', frameon = False)
+
+    plt.show()
+    
+    
+def orthoplot(data, speciesA, speciesB, titleA, titleB):
+    plt.rcParams['figure.dpi'] = 300
+    plt.rcParams['figure.figsize'] = [8, 8]
+    sns.set_style("whitegrid")
+    sns.scatterplot(data = data, x = speciesA, y = speciesB, 
+                size = 'Orthologs', sizes = (10, 200),
+                hue = 'Orthologs', palette = "crest")
+
+    plt.xlabel(titleA, fontsize = 13, 
+           labelpad = 15, style = 'italic')
+    plt.ylabel(titleB, fontsize = 13, 
+           labelpad = 15, style = 'italic')
+
+    plt.xticks(rotation='vertical', fontsize = 9)
+    plt.yticks(fontsize = 8)
+
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper left', 
+           fontsize=10, title = 'Orthologous genes', frameon = False)
+
+    plt.show()
